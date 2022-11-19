2a174bd3f05643c6703f37d82f26a80c7d266ce3
heheer <1239331448@qq.com>
2022-11-19 18:55:31 +0800

    photo component

--- blog/pages/photos.js
+++ blog/pages/photos.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import Header from "../components/Header";
+import Photos from "../components/Photo"
 
 export default function Photos() {
     return (
         <div>
             <Header />
             <div className="text-white text-4xl">
-                photos
+                <div>
+                    <Photos />
+                </div>
             </div>
         </div>
     )
